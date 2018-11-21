b5241c13fcdf60e7ea3f8a629d8bb1c8a06bff95
Givailson Neves <givailsonneves@wizsolucoes.com.br>
2018-11-21 18:11:46 -0200

    montagem da tela de registro

--- app/src/main/java/br/com/givailson/sebraelikeaboss/MainActivity.java
+++ app/src/main/java/br/com/givailson/sebraelikeaboss/MainActivity.java
@@ -2,20 +2,23 @@ package br.com.givailson.sebraelikeaboss;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
-import java.util.Date;
 import java.util.List;
 
 import br.com.givailson.sebraelikeaboss.database.Participant;
 import br.com.givailson.sebraelikeaboss.database.ParticipantViewModel;
+import br.com.givailson.sebraelikeaboss.views.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private ParticipantViewModel participantViewModel;
+    private View btRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +26,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         prepareDatabase();
+        btRegister = findViewById(R.id.btRegister);
+        btRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startRegistry();
+            }
+        });
 
-        Participant participant = new Participant("Givailson", "givailson@gmail.com", "TI", "xii", new Date());
+//        Participant participant = new Participant("Givailson", "givailson@gmail.com", "TI", "xii", new Date());
+//        participantViewModel.insert(participant);
+    }
 
-        participantViewModel.insert(participant);
+    private void startRegistry() {
+
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 
     private void prepareDatabase() {
